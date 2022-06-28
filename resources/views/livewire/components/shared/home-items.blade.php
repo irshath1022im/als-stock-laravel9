99ce0bf0772d7f81cacaa8ba8ca0ca805677a587
irshath1022im <im.irshath1022@gmail.com>
2022-06-28 13:03:00 +0300

    home items card body

--- resources/views/livewire/components/shared/home-items.blade.php
+++ resources/views/livewire/components/shared/home-items.blade.php
@@ -22,17 +22,18 @@
 
                     <div class="card mb-2" style="width: 18rem">
                         <div class="card-header">
-                            <div class="card-title text-uppercase">{{ $item->id }} / {{ $item->item }}</div>
+                            <div class="card-title text-uppercase">{{ $item->item }}</div>
                         </div>
-                        <div class="border borde-primary p-1" style="width: 250px; height:200px">
-                            <img class="card-img-top" style="width: 250px; height:200px" src="{{ Storage::URL($item->thumbnail)}}">
-                        </div>
+                        
                             
                         <div class="card-body">
-                            <h4 class="card-title">{{  $item->item }}</h4>
+                            <div class="border borde-primary p-1" style="width: 250px; height:200px">
+                                <img class="card-img-top" style="width: 250px; height:200px" src="{{ Storage::URL($item->thumbnail)}}">
+                            </div>
+                         
                         </div>
                         <div class="card-footer">
-                            <a name="" id="" class="btn btn-primary" href="{{ route('items.show',['item' => $item->id]) }}" role="button">View Item</a>
+                            <a name="" id="" class="btn btn-primary btn-sm" href="{{ route('items.show',['item' => $item->id]) }}" role="button">View Item</a>
                         </div>
 
                     </div>
